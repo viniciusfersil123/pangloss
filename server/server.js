1f0ce0cf5035a979ec475928cb45f5e621a54de9
viniciusfersil123 <viniciusfersil@gmail.com>
2025-07-23 22:09:37 +0200

    arruma server 4

--- server/server.js
+++ server/server.js
@@ -12,9 +12,9 @@ app.use(express.json());
 
 // Serve static files from React
 app.use(express.static(path.join(__dirname, 'client/build')));
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+// });
 
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
