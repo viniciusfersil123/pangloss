0a6125626133fd3da8561ff0add738032b5a49eb
viniciusfersil123 <viniciusfersil@gmail.com>
2025-07-23 22:15:47 +0200

    arruma server 6

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 // Serve static files from React
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, '../client/build')));
 // app.get('*', (req, res) => {
 //   res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 // });
